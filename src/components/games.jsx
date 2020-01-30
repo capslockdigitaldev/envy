e039fed59257deaf4d7ca9393a9929a792b529a9
asmitawalia <asmitawalia15@gmail.com>
2020-01-30 15:08:37 +0530

    updated

--- src/components/games.jsx
+++ src/components/games.jsx
@@ -74,7 +74,7 @@ class games extends Component {
                         }
                       };
                       const url =
-                "https://us.battle.net/oauth/check_token";
+                "https://us.battle.net/oauth/userinfo";
               axios.get(url, qs.stringify(postData), axiosConfig).then(res => {
             console.log(res)
             console.log('jjjj')
